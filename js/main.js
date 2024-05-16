204fda0a426f7e2a3bcf3b3278409a40d772eace
Ahmed417390 <ae417390@gmail.com>
2024-05-17 01:39:38 +0300

    e3

--- js/main.js
+++ js/main.js
@@ -1,44 +1,38 @@
 
 
-
-
-
-
 const quotes = [
-    {
-      name: "Jim Rohn",
-      quote: "Beware of what you become in pursuit of what you want."
-    },
-    {
-      name: "Epictetus",
-      quote: "It's not what happens to you, but how you react to it that matters."
-    },
-    {
-      name: "Frank Sinatra",
-      quote: "The best revenge is massive success."
-    },
-    {
-      name: "Wayne Gretzky",
-      quote: "You miss 100% of the shots you don't take."
-    },
-    {
-      name: "Nelson Mandela",
-      quote: "Resentment is like drinking poison and waiting for your enemies to die."
-    },
-    {
-      name: "Elbert Hubbard",
-      quote: "Do not take life too seriously. You will not get out alive."
-    }
-  ];
-  
-  function display() {
-      const randomIndex = Math.floor(Math.random() * quotes.length);
-      const quote = quotes[randomIndex];
-      document.getElementById('say').innerHTML = `" ${quote.quote}"`;
-      document.getElementById('name').innerHTML = `" ${quote.name}"`;
-    }
+  {
+    name: "Jim Rohn",
+    quote: "Beware of what you become in pursuit of what you want."
+  },
+  {
+    name: "Epictetus",
+    quote: "It's not what happens to you, but how you react to it that matters."
+  },
+  {
+    name: "Frank Sinatra",
+    quote: "The best revenge is massive success."
+  },
+  {
+    name: "Wayne Gretzky",
+    quote: "You miss 100% of the shots you don't take."
+  },
+  {
+    name: "Nelson Mandela",
+    quote: "Resentment is like drinking poison and waiting for your enemies to die."
+  },
+  {
+    name: "Elbert Hubbard",
+    quote: "Do not take life too seriously. You will not get out alive."
+  }
+];
 
-
+function display() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    document.getElementById('say').innerHTML = `" ${quote.quote}"`;
+    document.getElementById('name').innerHTML = `" ${quote.name}"`;
+  }
 
 
 
